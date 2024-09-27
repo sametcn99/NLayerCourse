a55f53e72367139abba7c140d3aae04d1611cefa
samet <sametcn99@gmail.com>
2024-09-28 01:20:58 +0300

    Add project files.

---
+++ Repositories/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace App.Repositories;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+{
+    public DbSet<Product> Products { get; set; } = default!;
+
+    /// <summary>
+    /// Configures the model for the database context.
+    /// </summary>
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
+
+        base.OnModelCreating(modelBuilder);
+
+    }
+}
